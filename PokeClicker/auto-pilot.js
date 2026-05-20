929ccc04b394fecf69b6a3dfd20e21b6e3d8a170
Zelow79 <zelow79@gmail.com>
2026-05-19 20:52:41 -0400

    Update auto-pilot.js

--- PokeClicker/auto-pilot.js
+++ PokeClicker/auto-pilot.js
@@ -13,7 +13,7 @@ document.addEventListener('keydown', function(event) {
             new Audio("https://www.myinstants.com/media/sounds/pokemon-redblueyellow-run-away-sound-effect.mp3").play();
         }
     }
-    // Numpad0 to toggle auto miner
+    // Numpad1 to toggle auto miner
     // currently only uses bombs when no objects are seen
     if (event.code === "Numpad1") { // code if Numpad 1 is pressed
         event.preventDefault();
@@ -24,6 +24,11 @@ document.addEventListener('keydown', function(event) {
         } else {
             new Audio("https://www.myinstants.com/media/sounds/cave11_0QWMESM.mp3").play();
         }
+    }
+    // Numpad9 to fire nukeMine, should complete the whole board instantly using the chisle (assumes infinite cooldown on chisle)
+    if (event.code === "Numpad9") { // code if Numpad 9 is pressed
+        event.preventDefault();
+        nukeMine();
     }
 });
 function clickAttack() {
@@ -43,6 +48,19 @@ function bombsAhoy() {
     }
     if (App.game.underground.battery.charges === App.game.underground.battery.maxCharges) {
         App.game.underground.battery.discharge(); // if discharge is ready use it
+    }
+}
+function nukeMine() {
+    let lastKnownFound = App.game.underground.mine.itemsFound; // track last know found value
+    for (let y = 0; y < App.game.underground.mine.height; y++) { // iterate mining grid
+        for (let x = 0; x < App.game.underground.mine.width; x++) {
+            const found = App.game.underground.mine.itemsFound; // get current found value
+            if (found < lastKnownFound) return; // kill function if board resets
+            lastKnownFound = App.game.underground.mine.itemsFound; // if function didn't die update last known found value
+            for (let i = 0; i < 3; i++) { // iterate 3 times to click tile 3 times
+                App.game.underground.tools.useTool(0, x, y); // clicking function giving it x and y coords for click 0 = chisle
+            }
+        }
     }
 }
 const zeWorker = setInterval(() => { // use functions on interval
